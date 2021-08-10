4289238ff3146001ab45f9f53410adc317982657
João Pedro Sousa <jassadosgame@live.com>
2021-08-10 16:57:16 -0300

    commit

---
+++ scripttext.js
@@ -0,0 +1,54 @@
+// const img = document.querySelector('img');
+
+// function callback(event) {
+//     console.log(event); 
+// }
+
+// // img.addEventListener(`click`, callback);
+    
+
+// const imagensLista = document.querySelector(`.animais-lista`);
+
+// function callbacklista(event){
+//     console.log(event.currentTarget)
+//     console.log(event.target)
+// }
+
+// imagensLista.addEventListener(`click`, callbacklista)
+
+// console.log(imagensLista);
+
+// const linkExeterno = document.querySelector('a[href^="http"]');
+
+// function handleLinkExterno() {
+//     alert(`garotasso`);
+// }
+
+// linkExeterno.addEventListener('click', handleLinkExterno)
+
+// const h1 = document.querySelector('h1');
+
+// function handleEvent(event){
+//     console.log(event.type)
+// }
+
+// h1.addEventListener('click', handleEvent);
+
+// function handKeyboard(event){
+//     console.log(event.key)
+// }
+
+// window.addEventListener('keydown', handKeyboard);
+
+
+// function handleIMG(event3){
+//     // console.log(event3.target);
+//     console.log(event3.currentTarget.getAttribute('src'));
+// }
+
+// imgs.forEach((img) => {
+//     img.addEventListener('click', handleIMG)
+    
+// });
+
+// let linksInternos = document.querySelectorAll('a[href^="#"]');
